0face60abbcd3a0bc42873d9d4a046cbb1a248fb
lucktige <2380384797@qq.com>
2022-06-25 11:12:25 +0800

    feat: 桂潇 -> [treehole核心功能模块构建]

---
+++ treeholecore/src/main/java/net/com/treehole/treeholecore/TreeholecoreApplication.java
@@ -0,0 +1,13 @@
+package net.com.treehole.treeholecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeholecoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeholecoreApplication.class, args);
+    }
+
+}
---
+++ treeholecore/src/test/java/net/com/treehole/treeholecore/TreeholecoreApplicationTests.java
@@ -0,0 +1,13 @@
+package net.com.treehole.treeholecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeholecoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
